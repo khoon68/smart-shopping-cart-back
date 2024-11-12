16ca4bcde335b1d1071a33d8077ae473ad674c16
khoon68 <126033125+khoon68@users.noreply.github.com>
2024-11-13 08:38:36 +0900

    초기세팅

---
+++ smart-shopping-cart-server/src/main/java/com/_k/smart_shopping_cart_server/SmartShoppingCartServerApplication.java
@@ -0,0 +1,13 @@
+package com._k.smart_shopping_cart_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartShoppingCartServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartShoppingCartServerApplication.class, args);
+	}
+
+}
---
+++ smart-shopping-cart-server/src/test/java/com/_k/smart_shopping_cart_server/SmartShoppingCartServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com._k.smart_shopping_cart_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartShoppingCartServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
